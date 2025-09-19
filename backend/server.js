0079208cb6ea688ec1284758af77ebbabe9b39b6
Mpho Qaba <mphoqaba1@gmail.com>
2025-09-19 14:26:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -105,4 +105,4 @@ app.post('/transactions', (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at https://wings-backend-gsej.onrender.com`));
